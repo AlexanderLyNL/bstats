07b34bbb43d6e429dfd53819a0451788ead9b662
Alexander Ly <Alexander.Ly.NL@gmail.com>
2020-07-09 14:15:00 +0200

    replicationBF set up for Kendall and Pearson via posteriorRhoFunc

--- R/kendall.R
+++ R/kendall.R
@@ -10,35 +10,65 @@ stretchedBetaTauSymmetric <- function(tauPop, betaA=1) {
   return(result)
 }
 
-
-priorTau <- function(tauPop, kappa=1, alternative="two.sided") {
-  if (alternative == "two.sided") {
-    priorLine <- stretchedBetaTauSymmetric(tauPop, betaA = 1/kappa)
-  } else if (alternative == "greater") {
-    priorLine <- priorTauPlus("tauPop"=tauPop, "kappa"=kappa)
-  } else if (alternative == "less") {
-    priorLine <- priorTauMin("tauPop"=tauPop, "kappa"=kappa)
-  }
+priorTau <- function(tauPop, kappa=1, alternative="two.sided", betaA=NULL, betaB=NULL) {
+  priorLine <- switch(alternative,
+                      "two.sided"=priorTauTwoSided("tauPop"=tauPop, "kappa"=kappa, "betaA"=betaA, "betaB"=betaB),
+                      "greater"=priorTauPlus("tauPop"=tauPop, "kappa"=kappa, "betaA"=betaA, "betaB"=betaB),
+                      "less"=priorTauMin("tauPop"=tauPop, "kappa"=kappa, "betaA"=betaA, "betaB"=betaB))
   return(priorLine)
 }
 
-priorTauPlus <- function(tauPop, kappa=1) {
+priorTauTwoSided <- function(tauPop, kappa=1, betaA=NULL, betaB=NULL) {
+  if (is.null(betaA) || is.null(betaB))
+    result <- stretchedBetaTauSymmetric("tauPop"=tauPop, "betaA" = 1/kappa)
+  else
+    result <- stretchedBetaTau("tauPop"=tauPop, "betaA"=betaA, "betaB"=betaB)
+
+  return(result)
+}
+
+priorTauPlus <- function(tauPop, kappa=1, betaA=NULL, betaB=NULL) {
   nonNegativeIndex <- tauPop >= 0
   lessThanOneIndex <- tauPop <= 1
   valueIndex <- as.logical(nonNegativeIndex*lessThanOneIndex)
   result <- tauPop*0
-  result[valueIndex] <- 2*priorTau(tauPop[valueIndex], kappa)
+
+  if (is.null(betaA) || is.null(betaB))
+    normalisationConstant <- 1/2
+  else
+    normalisationConstant <- pbeta(0.5, "shape1"=betaA, "shape2"=betaB, "lower.tail"=FALSE)
+
+  if (is.null(betaA) || is.null(betaB)) {
+    result[valueIndex] <- stretchedBetaTauSymmetric("tauPop"=tauPop[valueIndex],
+                                                    "betaA" = 1/kappa)/normalisationConstant
+  } else {
+    result[valueIndex] <- stretchedBetaTau("tauPop"=tauPop[valueIndex], "betaA"=betaA,
+                                           "betaB"=betaB)/normalisationConstant
+  }
   return(result)
 }
 
-priorTauMin <- function(tauPop, kappa=1) {
+priorTauMin <- function(tauPop, kappa=1, betaA=NULL, betaB=NULL) {
   negativeIndex <- tauPop <= 0
   greaterThanMinOneIndex <- tauPop >= -1
   valueIndex <- as.logical(negativeIndex*greaterThanMinOneIndex)
   result <- tauPop*0
-  result[valueIndex] <- 2*priorTau(tauPop[valueIndex], kappa)
+
+  if (is.null(betaA) || is.null(betaB))
+    normalisationConstant <- 1/2
+  else
+    normalisationConstant <- pbeta(0.5, "shape1"=betaA, "shape2"=betaB, "lower.tail"=TRUE)
+
+  if (is.null(betaA) || is.null(betaB)) {
+    result[valueIndex] <- stretchedBetaTauSymmetric("tauPop"=tauPop[valueIndex],
+                                                    "betaA" = 1/kappa)/normalisationConstant
+  } else {
+    result[valueIndex] <- stretchedBetaTau("tauPop"=tauPop[valueIndex], "betaA"=betaA,
+                                           "betaB"=betaB)/normalisationConstant
+  }
   return(result)
 }
+
 
 # 2. Bayes factors for Kendall's Tau -------------
 # These are the functions used for to compute the Bayes factors
@@ -172,41 +202,59 @@ bfKendallTauSavageDickey <- function(n, tauObs, kappa=1, var=1, h0=0, alternativ
 }
 
 # 3. Posteriors ---------
-# These are the functions used for to the posteriors
+# These are the functions used for the posteriors
 #
-posteriorTauU <- function(n, Tstar, tauPop, kappa=1, var=1, alternative="two.sided") {
-  #
-  result <- stats::dnorm("x"=Tstar, "mean"=(1.5*tauPop*sqrt(n)), "sd"=sqrt(var)) *
-    priorTau("tauPop"=tauPop, "kappa"=kappa, "alternative"=alternative )
+posteriorTauU <- function(n, tStar, nu=NULL, kappa=1, betaA=NULL, betaB=NULL, var=1,
+                          alternative="two.sided") {
+  result <- function(tauPop) {
+    tempResult <- 0
+
+    for (i in seq_along(n))
+      tempResult <- tempResult + stats::dnorm("x"=tStar[i], "mean"=(1.5*tauPop*sqrt(n[i])), "sd"=sqrt(var), log=TRUE)
+
+    tempResult <- exp(tempResult)*priorTau("tauPop"=tauPop, "kappa"=kappa, "alternative"=alternative,
+                                           "betaA"=betaA, "betaB"=betaB)
+    return(tempResult)
+  }
   return(result)
 }
 
-posteriorTau <- function(n, tauObs, tauPop, kappa=1, var=1, alternative="two.sided") {
-  Tstar <- (tauObs * ((n*(n-1))/2))/sqrt(n*(n-1)*(2*n+5)/18)
+makePosteriorTauFunc <- function(n, tauObs, kappa=1, var=1, alternative="two.sided",
+                             betaA=NULL, betaB=NULL) {
+
+  tStar <- vector("numeric", length=length(n))
+
+  for (i in seq_along(n))
+    tStar[i] <- (tauObs[i] * ((n[i]*(n[i]-1))/2))/sqrt(n[i]*(n[i]-1)*(2*n[i]+5)/18)
+
   lims <- switch(alternative,
                  "two.sided"=c(-1, 1),
                  "greater"=c(0, 1),
                  "less"=c(-1, 0)
   )
 
-  logicalCensor <- (tauPop >= lims[1] && tauPop <= lims[2])
+  integrandF <- posteriorTauU("n"=n, "tStar"=tStar, "kappa"=kappa, "betaA"=betaA,
+                              "betaB"=betaB, "var"=var, "alternative"=alternative)
 
-  integrand <- function(x) {
-    posteriorTauU("n"=n, "Tstar"=Tstar, "tauPop"=x, "kappa"=kappa, "var"=var, "alternative"=alternative)
-  }
-  normalisingConstant <- try(silent=TRUE, integrate(integrand, lims[1], lims[2])[["value"]])
+  normalisingConstant <- try(silent=TRUE, integrate(integrandF, lims[1], lims[2])[["value"]])
 
-  if (isTryError(normalisingConstant)) {
+  if (isTryError(normalisingConstant))
     result <- NA
-  } else {
-    # I could also do
-    result <- integrand(tauPop)/normalisingConstant
-  }
+  else
+    result <- function(tauPop){integrandF(tauPop)/normalisingConstant}
+
   return(result)
 }
 
+posteriorTau <- function(n, tauObs, tauPop, kappa=1, var=1, alternative="two.sided",
+                         betaA=NULL, betaB=NULL) {
+  posteriorTauF <- makePosteriorTauFunc("n"=n, "tauObs"=tauObs, "kappa"=kappa, "var"=var,
+                                        "alternative"=alternative, "betaA"=betaA,
+                                        "betaB"=betaB)
+  posteriorTauF(tauPop)
+}
 
-computeKendallCredibleInterval <- function(n, tauObs, kappa=1, var=1, ciValue=0.95, h0=0) {
+computeKendallCredibleInterval <- function(n, tauObs, kappa=1, var=1, ciValue=0.95, h0=0, betaA=NULL, betaB=NULL) {
   # Compute Kendall's correlation credible interval based on a sampling
   #
   check <- failIfNot(ciValue > 0, ciValue < 1, !isSomeNull(n, tauObs))
@@ -219,20 +267,25 @@ computeKendallCredibleInterval <- function(n, tauObs, kappa=1, var=1, ciValue=0.
 
   for (alternative in c("two.sided", "greater", "less")) {
     result[[alternative]] <- credibleIntervalKendallTauSingle("n"=n, "tauObs"=tauObs, "kappa"=kappa, "var"=var,
-                                                               "alternative"=alternative, "ciValue"=ciValue)
+                                                               "alternative"=alternative, "ciValue"=ciValue,
+                                                              "betaA"=betaA, "betaB"=betaB)
   }
 
   return(result)
 }
 
-# Compute credible intervals kendalls tau
-credibleIntervalKendallTauSingle <- function(n, tauObs, kappa=1, var=1, alternative="two.sided", ciValue = 0.95, m=4000) {
+# SINGLE refers to the side
+credibleIntervalKendallTauSingle <- function(n, tauObs, kappa=1, var=1, alternative="two.sided",
+                                             ciValue = 0.95, m=4000, betaA=NULL, betaB=NULL) {
   # TODO(Alexander): Interesting use-case: n=800, tauObs=-0.8, m=1000
+  posteriorTauFunc <- makePosteriorTauFunc("n"=n, "tauObs"=tauObs, "kappa"=kappa, "var"=var,
+                                           "alternative"=alternative, "betaA"=betaA, "betaB"=betaB)
+
   lowCI <- (1-ciValue)/2
   upCI <- (1+ciValue)/2
   tauPopDomain <- seq(-1, 1, length.out=(m-1))
-  densVals <- posteriorTau("n"=n, "tauObs"=tauObs, "tauPop"=tauPopDomain, "kappa"=kappa, "var"=var,
-                            "alternative"=alternative)
+
+  densVals <- posteriorTauFunc(tauPopDomain)
   cdfVals <- cumsum((densVals[1:(m-1)] + densVals[2:m]) * 0.5 * (tauPopDomain[2]-tauPopDomain[1]))
 
   lowerCi <- tauPopDomain[which(cdfVals>=lowCI)[1]]
